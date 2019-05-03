763e4e6a7f2e0be5f602baf42d2d1fed11f8ded2
richardbrian <richardbrian.23@gmail.com>
2019-05-03 10:43:03 +0530

    Update SnakeAndLadder.py

--- SnakeAndLadder.py
+++ SnakeAndLadder.py
@@ -1,13 +1,12 @@
 import random
 import time
 
-class SnakeAndLadderBoard(object):
+
+class LudoBoard(object):
     def __init__(self, boardSize):
         self.boardSize = boardSize
         self.cellOccupied = {}
-        self.ladderLength = [0,0,0,5,15,10]
-        self.snakeLength = [5,10,15]
-        self.dice = [1,2,3,4,5,6]
+        self.ladderLength = [0,0,0,5,7,9 ]
         self.boardLayout = []
 
         self._createBoard()
@@ -19,6 +18,10 @@ class SnakeAndLadderBoard(object):
     def _createBoard(self):
         for i in range(self.boardSize):
            self.boardLayout.append(0)
+
+    def _printBoard(self):
+        for index, value  in enumerate(self.boardLayout):
+            print("%s=%s" % (index, value))
 
     def _addLadder(self):
        #Add Ladders
@@ -39,15 +42,11 @@ class SnakeAndLadderBoard(object):
           else:
               self.boardLayout[index] = 0
 
-    def _printBoard(self):
-        for index, value  in enumerate(self.boardLayout):
-            print("%s=%s" % (index, value))
-
     def _addSnake(self):
-
         for reverseIndex, value in reversed(list(enumerate(self.boardLayout))):
             cellValue = random.choice(self.ladderLength)
             if reverseIndex - cellValue > 0:
+                # If cell value is less than 0, that it points to non existing cells.
                 if cellValue > 0:
                     # Cell is occupied either by ladder head or ladder foot.
                     if reverseIndex - cellValue in self.cellOccupied:
@@ -58,6 +57,7 @@ class SnakeAndLadderBoard(object):
                         continue
                     self.cellOccupied[reverseIndex] = 1
                     self.cellOccupied[reverseIndex - cellValue] = 1
+                    # Add a negative number such that its easy to subtract
                     self.boardLayout[reverseIndex] = cellValue * (-1)
 
     def movePlayer(self, currentPosition, diceRoll):
@@ -66,13 +66,15 @@ class SnakeAndLadderBoard(object):
         if currentPosition + diceRoll < self.boardSize:
             newPosition = currentPosition + diceRoll
             if self.boardLayout[newPosition] > 0:
-                print("Great Luck got a ladder of steps %s" % self.boardLayout[newPosition])
+                print("Great Luck!! Got a ladder. Jump %s steps ahead." % self.boardLayout[newPosition])
                 time.sleep(2)
             elif self.boardLayout[newPosition] < 0:
-                print("Bad luck, Encountered a snake. Go aback %s steps" % (-1 * self.boardLayout[newPosition]))
+                print("Bad luck!! Encountered a Snake. Go back %s steps" % (-1 * self.boardLayout[newPosition]))
                 time.sleep(2)
             newPosition += self.boardLayout[newPosition]
-
+            return newPosition
+        elif currentPosition + diceRoll == self.boardSize:
+            newPosition = currentPosition + diceRoll
             return newPosition
         else:
             return currentPosition
@@ -89,30 +91,32 @@ def gameLogic(playerObj, gameObj, playerName):
     time.sleep(1)
     playerObj.pos = gameObj.movePlayer(playerObj.pos, rollDice)
     if rollDice == 6:
-        print("Awesome!! 6 .. Once more chance to roll the dice")
+        print("Awesome!! 6 .. One more chance to roll the dice")
         time.sleep(1)
         gameLogic(playerObj, gameObj, playerName)
     return  playerObj.pos
 
 def main(boardSize):
-    ludogame=SnakeAndLadderBoard(boardSize)
+    ludogame=LudoBoard(boardSize)
     p1= Player()
     p2= Player()
     print("Starting the game")
-    while p1.pos != boardSize-1 and p1.pos != boardSize-1:
+    while p1.pos != boardSize and p1.pos != boardSize:
         position = gameLogic(p1, ludogame, "P1")
         print("Player1 position = %s" % position)
         print("---------------------------------------")
+        if p1.pos == boardSize:
+            print("P1 winner")
+            break
         time.sleep(1)
         position = gameLogic(p2, ludogame, "P2")
         print("Player2 position = %s" % position)
         print("---------------------------------------")
 
-    if p1.pos == boardSize-1:
-        print("p1 winner")
-    elif p2.pos == boardSize-1 :
-        print("p2 winner")
+        if p2.pos == boardSize :
+           print("P2 winner")
+           break
 
 if __name__ == "__main__":
-    boardSize = 10000
+    boardSize = 100
     main(boardSize)
